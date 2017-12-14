7176d78a3165c10a9d3a8f719fd145501588133e
Laura <wardla2@gmail.com>
2017-12-14 13:18:31 -0700

    pretty much done example of class

--- bookstore_class.py
+++ bookstore_class.py
@@ -1,58 +1,59 @@
 class Bookstore(object):
-    def __init__(self, bookstore):
-        self.bookstore = bookstore
+    def __init__(self, name):
+        self.bookstore = self.create_bookstore(name)
     
-    def create_bookstore(name):
+    def create_bookstore(self, name):
         return {'bookstore_name': name,
             'authors' : [],
             'books' : [],
             'next_author_id' : 0,
             'next_book_id' : 0}
 
-    def get_bookstore_name(bookstore):
-        return bookstore['bookstore_name']
+    def get_bookstore_name(self):
+        return self.bookstore['bookstore_name']
 
 
-    def add_author(bookstore, name, nationality):
-        author = { 'id' : bookstore['next_author_id'],
+    def add_author(self, name, nationality):
+        author = { 'id' : self.bookstore['next_author_id'],
                'name' : name,
                'nationality' : nationality}
-        bookstore['authors'].append(author)
-        bookstore['next_author_id'] +=1
+        self.bookstore['authors'].append(author)
+        self.bookstore['next_author_id'] +=1
         return author
  
 
-    def get_author_by_name(bookstore, name):
-        for author in bookstore['authors']:
+    def get_author_by_name(self, name):
+        for author in self.bookstore['authors']:
             if name == author['name']:
                 return author
         return None
 
 
-    def add_book(bookstore, title, isbn, author):
-        book = { 'id' : bookstore['next_book_id'],
+    def add_book(self, title, isbn, author):
+        book = { 'id' : self.bookstore['next_book_id'],
              'title' : title,
              'isbn' : isbn,
              'author' : get_author_by_name(bookstore, author)['name']}
-        bookstore['books'].append(book)
-        bookstore['next_book_id'] += 1
+        self.bookstore['books'].append(book)
+        self.bookstore['next_book_id'] += 1
         return book
 
 
-    def get_book_by_title(bookstore, title):
-        for book in bookstore['books']:
+    def get_book_by_title(self, title):
+        for book in self.bookstore['books']:
             if title == book['title']:
                 return book
         return None
 
 
-    def get_books_by_author(bookstore, author):
+    def get_books_by_author(self, author):
         booklist = []
-        for book in bookstore['books']:
+        for book in self.bookstore['books']:
             if author == book['author']:
                 booklist.append(book)
         return booklist
     
-b1 = Bookstore()
-    #bookstore = create_bookstore(name)
- 
+b1 = Bookstore('the_bookstore')
+print(b1.get_bookstore_name())
+print(b1.add_author('Elena Ferrante', 'Italy'))
+
